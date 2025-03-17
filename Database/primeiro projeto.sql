e984a05e1a3ccaafe7c2868279ae54a7237d205c
Gabriel Tavares <gabriel.tbm16@gmail.com>
2025-03-17 11:33:18 -0300

    Update primeiro projeto.sql
    atualização do primeiro projeto

--- Database/primeiro projeto.sql
+++ Database/primeiro projeto.sql
@@ -11,3 +11,35 @@ create table cliente(
     cartao varchar(16),
     primary key (id_cliente)
 );
+
+create table funcionario(
+	id_funcionario int unsigned auto_increment not null,
+	nome varchar(50) not null,
+    salario decimal(10, 2) not null default '0',
+    departamento varchar(45) not null,
+    primary key (id_funcionario)
+);
+
+create table veiculo(
+	id_veiculo int unsigned auto_increment not null,
+	marca varchar(50) not null,
+    placa_veiculo varchar(7) unique not null,
+    modelo_veiculo varchar(50) not null,
+    valor decimal(10, 2) not null,
+    ano_fabricacao int not null check (ano_facbricacao >= 2010),
+    status enum ('Disponivel' , 'Vendido') not null default 'Disponivel',
+    primary key (id_veiculo)
+);
+
+create table venda(
+	id_venda int unsigned auto_increment not null,
+    valor_venda decimal(10, 2) not null,
+    data_venda date not null,
+    id_cliente int unsigned not null,
+    id_fincionario int unsigned not null,
+    id_veiculo int unsigned not null,
+    constraint fk_funcionario_venda foreign key (id_funcionario) references funcionario (id_funcionario),
+    constraint fk_veiculo_venda foreign key (id_veiculo) references veiculo (id_veiculo),
+    constraint fk_cliente_venda foreign key (id_cliente) references cliente (id_cliente),
+    primary key (id_venda)
+);
