e0f72ae3d51e41bdc2a5afd74c7c737bae441b45
Gabriel Tavares <gabriel.tbm16@gmail.com>
2025-03-24 10:41:18 -0300

    adicionando o projeto corrigido
    atualizações da ultima aula de banco de dados

--- Database/primeiro projeto.sql
+++ Database/primeiro projeto.sql
@@ -43,3 +43,62 @@ create table venda(
     constraint fk_cliente_venda foreign key (id_cliente) references cliente (id_cliente),
     primary key (id_venda)
 );
+
+insert into cliente (nome, cnh, cpf, tipo_cliente, cartao) values
+('Gabriel', '12345678901', '12376892467', 'Física', '4111111111111111'),
+('Exmpresa X', '0987654321', '11259733579', 'Juridica', '5222222222222'),
+('Adson', '1234512345', '09579240069', 'Física', '63333333333333'),
+('Sorriso', '0987609876', '86209529512', 'Juridica', '7444444444444');
+
+insert into funcionario (nome, salario, departamento) values
+('Carlos Souza', 2500.00, 'Vendas'),
+('Gabiel Tavares', 1500.00, 'Administração'),
+('Adson', 2100.00, 'Estoque'),
+('Sorriso', 1000.00, 'Vendas');
+
+insert into veiculo (marca, placa_veiculo, modelo_veiculo, valor, ano_fabricacao) values
+('Ford', 'DEF1234', 'Fiesta', 65000.00, 2018),
+('Toyota', 'XCV3456', 'Corolla', 123000.00, 2020),
+('Chevrolet', 'GTP5624', 'Camaro', 450000.00, 2019),
+('Ferrari', 'PTB1570', 'Ferrari Spider', 1500000.00, 2023);
+
+insert into venda (valor_venda, data_venda, fk_funcionario_venda, fk_veiculo_venda, fk_cliente_venda) values
+(25000.00, 2020, 1, 1, 1),
+(150000, 2019, 2, 2, 2),
+(75000, 2018, 3, 3, 3),
+(80500, 2023, 4, 4, 4);
+
+select * from cliente;
+select * from funcionario;
+select * from veiculo;
+select * from venda;
+
+SELECT
+	veiculo.marca, veiculo.modelo, veiculo.ano_fabricacao, veiculo.placa_veiculo,
+    veiculo.valor_venda, veiculo.data_venda,
+    cliente.nome AS nome_cliente,
+    funcionario.nome AS nome_funcionario
+FROM veiculo
+inner join venda on veiculo.id_veiculo = venda.id_veiculo
+inner join cliente on venda.id_cliente = cliente.id_cliente
+inner join funcionario on venda.id_funcionario = funcionario.id_funcionario;
+
+select avg(valor_venda) from venda;
+
+create view media_venda as
+select
+	round(avg(valor_venda), 2) as media_valor_venda,
+    count(*) as total_vendas
+from venda;
+
+select * from media_venda;
+
+create view media_vendas_por_funcionario as
+select
+	id_funcionario,
+    round(avg(valor_venda), 2) as media_valor_venda,
+    count(*) as total_vendas
+from venda
+group by id_funcionario;
+
+select * from media_vendas_por_funcionario;
